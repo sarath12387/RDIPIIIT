95354487e6b8198b94bf5a88584d802f490fe995
sarath12387 <56636472+sarath12387@users.noreply.github.com>
2020-09-01 11:39:38 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h2>JavaScript Arrays</h2>
+
+<p id="demo"></p>
+
+<script>
+var numbers = [1,2,3,4,5];
+var x=numbers.toString(3);
+
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
